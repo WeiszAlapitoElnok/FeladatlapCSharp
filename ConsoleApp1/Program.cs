193afee7c8b84c384954f8f60f228a61162b1ffd
Rendszergazda <rendszergazda@blathy.info>
2025-09-18 11:34:33 +0200

    feladatok 35-ig

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp1
+﻿using System.Text;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
@@ -377,8 +379,219 @@
 
         static void F26()
         {
+            int num = Num();
+            int oszto = 1;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    oszto++;
+                }
+            }
+            if (oszto == 2)
+            {
+                Console.WriteLine("prim");
+            }
+            else
+            {
+                Console.WriteLine("nem prim");
+            }
+        }
+
+        static void F27()
+        {
+            int num = Num();
+            int oszto = 1;
+
+            for (int i = 1; i < num; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        oszto++;
+                    }
+                }
+                if (oszto == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                oszto = 1;
+            }
+        }
+
+        static void F28()
+        {
+            int num = Num();
+            int oszto = 1;
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    for (int j = 1; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            oszto++;
+                        }
+                    }
+                    if (oszto == 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    oszto = 1;
+                }
+            }
+        }
+
+        static void F29()
+        {
+            int num = Num();
+            int oszto = 1;
+
+            while (num != 1)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        for (int j = 1; j < i; j++)
+                        {
+                            if (i % j == 0)
+                            {
+                                oszto++;
+                            }
+                        }
+                        if (oszto == 2)
+                        {
+                            Console.WriteLine($"{num}|{i}");
+                            num = num / i;
+                            i = 1;
+                        }
+                        oszto = 1;
+                    }
+                }              
+            }
+            Console.WriteLine($"{1}|");
+        }
+
+        static void F30()
+        {
+            int num = Num();
+            int num2 = Num();
+            int smalst = 0;
+
+            if (num < num2)
+            {
+                for (int i = num; i > 0; i--)
+                {
+                    if (num % i == 0 && num2 % i == 0)
+                    {
+                        if (smalst < i)
+                        {
+                            smalst = i;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = num2; i > 0; i--)
+                {
+                    if (num % i == 0 && num2 % i == 0)
+                    {
+                        if (smalst < i)
+                        {
+                            smalst = i;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(smalst);
+        }
+
+        static void F31()
+        {
+            int num = Num();
+            int num2 = Num();
+            int smalst = num*num2;
+
+            for (int i = 1; i < (num*num2); i++)
+            {
+                if (i % num == 0 && i % num2 == 0)
+                {
+                    if (i < smalst)
+                    {
+                        smalst = i;
+                    }
+                }
+            }
+            Console.WriteLine(smalst);
+        }
+
+        static void F32()
+        {
+            int num = Num();
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num}*{i}={num*i}");
+            }
+        }
+
+        static void F33()
+        {
+            int num = Num();
+            for (int i = 1; i <= num; i++)
+            {
+
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.WriteLine($"{i}+{j}={i + j}");
+                }
+            }
 
         }
+
+        static void F34()
+        {
+            int inv = 0;
+            int inv2 = 0;
+            for (int i = 10; i < 100; i++)
+            {
+                for (int j = 10; j < 100; j++)
+                {
+                    inv = i/10+(i%10) * 10;
+                    inv2 = j / 10 + (j % 10) * 10;
+                    if (j*i == inv * inv2)
+                    {
+                        Console.WriteLine($"{i},{j}");
+                    }
+                }
+            }
+        }
+
+        static void F35()
+        {
+            int sor = 0;
+            int start = 97;
+            do
+            {
+                for (int i = start; i < start + 4; i++)
+                {
+
+
+                    Console.Write($"{Convert.ToChar(i)}\t{i}\t");
+
+                }
+                Console.WriteLine();
+                start = start + 4;
+            } while (start < 122);
+
+
+
+        }
+
             static void Main(string[] args)
         {
         }
